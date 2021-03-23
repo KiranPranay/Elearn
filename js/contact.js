b6180f1753a22385f45399e06071d0c84c14e9fc
kiranpranay <kiranpranay12@gmail.com>
2021-03-23 16:55:59 +0530

    logo

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,12 @@
 // contact page
-document
-  .querySelector("#contact-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    e.target.elements.name.value = "";
-    e.target.elements.email.value = "";
-    e.target.elements.message.value = "";
-  });
+// document
+//   .querySelector("#contact-form")
+//   .addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     e.target.elements.name.value = "";
+//     e.target.elements.email.value = "";
+//     e.target.elements.message.value = "";
+//   });
 // maps
 $(document).ready(function () {
   $("body").noisy({
