94ef3532614b7de0205c047aaa9e0b57fd423cdf
kiranpranay <kiranpranay12@gmail.com>
2021-03-23 17:31:01 +0530

    greet 2021

--- mail.php
+++ mail.php
@@ -52,7 +52,7 @@
     </div>
 
     <footer class="site-footer" id="footer">
-        <p class="site-footer__fineprint" id="fineprint">Copyright ©2014 | All Rights Reserved</p>
+        <p class="site-footer__fineprint" id="fineprint">Copyright ©2021 | All Rights Reserved</p>
     </footer>
 </body>
 
@@ -82,15 +82,15 @@
         <h1 class="site-header__title" data-lead-id="site-header-title">FAILED</h1>
     </header>
 
-    <div class="main-content">
+    <!-- <div class="main-content">
         <i class="fa fa-check main-content__checkmark" id="checkmark"></i>
         <p class="main-content__body" data-lead-id="main-content-body">Thanks a bunch for filling that out. It means a
             lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for
             being you.</p>
-    </div>
+    </div> -->
 
     <footer class="site-footer" id="footer">
-        <p class="site-footer__fineprint" id="fineprint">Copyright ©2014 | All Rights Reserved</p>
+        <p class="site-footer__fineprint" id="fineprint">Copyright ©2021 | All Rights Reserved</p>
     </footer>
 </body>
 
